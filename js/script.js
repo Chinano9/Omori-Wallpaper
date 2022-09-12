610de678e554117335063a52b735dcd8e96800ea
chinano9 <ponceconc@outlook.com>
2022-09-11 20:16:52 -0600

    Codigo ordenado por clases

--- js/script.js
+++ js/script.js
@@ -1,48 +1,56 @@
-/*
-MIT License
+class Background {
+    constructor(canvas) {
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.imagenClaro = new Image();
+        this.imagenOscuro = new Image();
+        this.switchClaro = new Switch(125,183,900,300, 'res/switchClaro.png')
+        this.switchClaro = new Switch(125,183,900,300, 'res/switchOscuro.png')
+        this.imagenClaro.src = 'res/claro.png';
+        this.imagenOscuro.src = 'res/oscuro.png';
+        this.context = canvas.getContext('2d');
+    }
 
-Copyright (c) 2017 Pavel Dobryakov
+    /**
+     * 
+     * @param {MouseEvent} e 
+     * @returns void
+     */
+    colocarImagen(e) {
+        const x = e.clientX;
+        const y = e.clientY;
+        if (x > 400 && x < 500) {
+            
+            this.context.drawImage(this.imagenClaro, 0, 0, this.width, this.height);
+            return;
+        }
+        this.context.drawImage(this.imagenOscuro, 0, 0, this.width, this.height);
+    }
+}
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+class Switch{
+    constructor(width, height, xPosition, yPosition, src){
+        this.img = new Image();
+        this.img.src = src;
+        this.width = width;
+        this.height = height;
+        this.x = xPosition;
+        this.y = yPosition; 
+    }
+}
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+const canvas = document.querySelector('canvas');
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-'use strict';
-
-const canvas = document.getElementsByTagName('canvas')[0];
-const context = canvas.getContext('2d');
-const {width, height} = canvas;
-const claro = new Image();
-const oscuro = new Image();
-
-claro.src = 'https://cdn.discordapp.com/attachments/751980478420222022/1016920072041795624/ErqwWzTW4AErlW2.png';
-oscuro.src = 'https://cdn.discordapp.com/attachments/751980478420222022/1016920072385732718/ErqwXRxXAAE5tnL.png';
-
-claro.onload = (e)=>{
-    context.fillStyle = "#ff0000";
-    context.arc(10,10,40,30,50,false)
-    // context.drawImage(claro,0,0, Number(width), Number(height));
-};
+const background = new Background(canvas);
 
 
-window.addEventListener('mouseup', (e) => {
-    let x = e.clientX;
-    let y = e.clientY;
-    console.log(`${x} eje x`);
-    console.log(`${y} eje y`);
-});
+background.context.imageSmoothingEnabled = false;
+background.context.imageSmoothingQuality = "high";
+background.context.drawImage(background.imagenOscuro, 0, 0, background.width, background.height);
+
+
+
+window.addEventListener('mouseup', (e) => background.colocarImagen(e));
