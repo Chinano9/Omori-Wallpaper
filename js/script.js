8fbbdd9ecabd0a4748ab5eba8da8598fd9c54d36
chinano9 <ponceconc@outlook.com>
2022-09-23 18:27:59 -0600

    Solo falta hacer que al iniciar tenga la imagen

--- js/script.js
+++ js/script.js
@@ -1,44 +1,3 @@
-class Background {
-
-    constructor(canvas) {
-        this.width = canvas.width;
-        this.height = canvas.height;
-        this.imagenClaro = new Image();
-        this.imagenOscuro = new Image();
-        this.switchClaro = new Switch(25, 33, 900, 300, 'res/switchClaro.png')
-        this.switchOscuro = new Switch(25, 33, 900, 300, 'res/switchOscuro.png')
-        this.imagenClaro.src = 'res/claro.png';
-        this.imagenOscuro.src = 'res/oscuro.png';
-        this.context = canvas.getContext('2d');
-        this.bandera = true;
-    }
-
-    /**
-     * 
-     * @param {MouseEvent} e 
-     * @returns void
-     */
-    colocarImagen(e) {
-        const x = e.clientX;
-        const y = e.clientY;
-        const tocoX = x > this.switchClaro.x && x < (this.switchClaro.x + this.switchClaro.width);
-        const tocoY = y > this.switchClaro.y && y < (this.switchClaro.y + this.switchClaro.height);
-        if (tocoX && tocoY) {
-            this.bandera = !this.bandera;
-            if(this.bandera){
-                this.context.clearRect(0, 0, this.width, this.height);
-                this.context.drawImage(this.imagenClaro, 0, 0, this.width, this.height);
-                this.context.drawImage(this.switchClaro.img, this.switchClaro.x, this.switchClaro.y, this.switchClaro.width, this.switchClaro.height);
-                console.table(this.switchClaro);
-                return;
-            }
-            this.context.clearRect(0, 0, this.width, this.height);
-            this.context.drawImage(this.imagenOscuro, 0, 0, this.width, this.height);
-            this.context.drawImage(this.switchOscuro.img, this.switchOscuro.x, this.switchOscuro.y, this.switchOscuro.width, this.switchOscuro.height);
-        }
-    }
-}
-
 class Switch {
     constructor(width = 0, height = 0, xPosition = 0, yPosition = 0, src = "") {
         this.img = new Image();
@@ -55,16 +14,44 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const background = new Background(canvas);
+const width = canvas.width;
+const height = canvas.height;
+const imagenClaro = new Image();
+const imagenOscuro = new Image();
+const switchClaro = new Switch(25, 33, 900, 300, 'res/switchClaro.png')
+const switchOscuro = new Switch(25, 33, 900, 300, 'res/switchOscuro.png')
+imagenClaro.src = 'res/claro.png';
+imagenOscuro.src = 'res/oscuro.png';
+const context = canvas.getContext('2d');
+let bandera = false;
+
+/**
+ * 
+ * @param {MouseEvent} e 
+ * @returns void
+ */
+function colocarImagen(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    const tocoX = x > switchClaro.x && x < (switchClaro.x + switchClaro.width);
+    const tocoY = y > switchClaro.y && y < (switchClaro.y + switchClaro.height);
+    if (tocoX && tocoY) {
+        bandera = !bandera;
+    }
+    if (bandera){
+        context.clearRect(0, 0, width, height);
+        context.drawImage(imagenClaro, 0, 0, width, height);
+        context.drawImage(switchClaro.img, switchClaro.x, switchClaro.y, switchClaro.width, switchClaro.height);
+        return;
+    }
+    context.clearRect(0, 0, width, height);
+    context.drawImage(imagenOscuro, 0, 0, width, height);
+    context.drawImage(switchOscuro.img, switchOscuro.x, switchOscuro.y, switchOscuro.width, switchOscuro.height);
+}
 
 
-background.context.imageSmoothingEnabled = false;
-background.context.imageSmoothingQuality = "high";
 
-window.addEventListener('mouseup', (e) => background.colocarImagen(e));
+context.imageSmoothingEnabled = false;
+context.imageSmoothingQuality = "high";
 
-background.context.drawImage(background.imagenOscuro, 0, 0, background.width, background.height);
-
-
-
-
+window.addEventListener('mouseup', (e) => colocarImagen(e));
